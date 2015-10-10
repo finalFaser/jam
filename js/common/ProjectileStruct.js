5ce747433179f431e881728014bfc00dcdf9b3cc
unknown <zeratul.exe@gmail.com>
2015-10-10 11:32:11 +0100

    1st draft

---
+++ js/common/ProjectileStruct.js
@@ -0,0 +1,12 @@
+/**
+ * Created by jorge.graca on 10/10/2015.
+ */
+
+ProjectileStruct = function(){
+
+    return {
+        x : 0,
+        y : 0
+    };
+
+}
---
+++ js/server/main.js
@@ -0,0 +1,4 @@
+/**
+ * Created by jorge.graca on 10/10/2015.
+ */
+var http = require("http");
