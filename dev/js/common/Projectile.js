ed3d42a4dc53d0ee9f94593eee59c8cb2f492b5a
unknown <zeratul.exe@gmail.com>
2015-10-10 18:16:49 +0100

    Machingunnamos

--- dev/js/common/Projectile.js
+++ dev/js/common/Projectile.js
@@ -2,38 +2,27 @@
  * Created by jorge.graca on 10/10/2015.
  */
 
-// Define constants
-var SHOT_DELAY = 100; // milliseconds (10 bullets/second)
-var BULLET_SPEED = 500; // pixels/second
-var NUMBER_OF_BULLETS = 1;
 
-var Projectile = function(game){
+var BULLET_SPEED = 500; // pixels/second
+
+var Projectile = function(game, x, y){
     Phaser.Group.call( this, game );
 
-    this.bullet = this.create(300, 300, 'BalaPotassio');
+    this.bullet = this.create(x, y, 'BalaPotassio');//create a Sprite and add it to this(Group)
     this.game.physics.enable(this.bullet, Phaser.Physics.ARCADE);
     this.bullet.anchor.set(0.5,0.5);
+
+    this.bullet.kill();
 };
 
 Projectile.prototype = Object.create(Phaser.Group.prototype);
 
 Projectile.prototype.update = function() {
     Phaser.Group.prototype.update.call(this);
-
-    // Shoot a bullet
-    if (this.game.input.activePointer.isDown) {
-        this.shootBullet();
-    }
 };
 
-Projectile.prototype.shootBullet = function(rotation) {
-    // Enforce a short delay between shots by recording
-    // the time that each bullet is shot and testing if
-    // the amount of time since the last shot is more than
-    // the required delay.
-    if (this.lastBulletShotAt === undefined) this.lastBulletShotAt = 0;
-    if (this.game.time.now - this.lastBulletShotAt < this.SHOT_DELAY) return;
-    this.lastBulletShotAt = this.game.time.now;
+Projectile.prototype.shootBullet = function(x,y) {
+
 
     // Get a dead bullet from the pool
     //var bullet = this.bulletPool.getFirstDead();
@@ -43,7 +32,7 @@ Projectile.prototype.shootBullet = function(rotation) {
 
     // Revive the bullet
     // This makes the bullet "alive"
-    //bullet.revive();
+    this.bullet.revive();
 
     // Bullets should kill themselves when they leave the world.
     // Phaser takes care of this for me by setting this flag
@@ -53,7 +42,7 @@ Projectile.prototype.shootBullet = function(rotation) {
     this.bullet.outOfBoundsKill = true;
 
     // Set the bullet position to the gun position.
-    //bullet.reset(this.gun.x, this.gun.y);
+    this.bullet.reset(x, y);
 
     // Aim the gun at the pointer.
     // All this function does is calculate the angle using
--- dev/js/states/play.js
+++ dev/js/states/play.js
@@ -1,12 +1,33 @@
 var Projectile = require('..\\common\\Projectile');
 
+// Define constants
+var SHOT_DELAY = 100; // milliseconds (10 bullets/second)
+var NUMBER_OF_BULLETS = 100;
+
+
+var iBullet = 0;
+
 module.exports = {
     create: function(){
-        //This is just like any other Phaser create function
-        this.teste = new Projectile(this.game);
+        this.lastBulletShotAt = 0;
+        this.bulletPool = this.game.add.group();
+        for(var i = 0; i < NUMBER_OF_BULLETS; i++) {
+            this.bulletPool.add(new Projectile(this.game, 0, 0));
+        }
 
     },
     update: function(){
-        //Game logic gpdate(;
+
+        // Shoot a bullet
+        if (this.game.input.activePointer.isDown) {
+            // Enforce a short delay between shots by recording
+            // the time that each bullet is shot and testing if
+            // the amount of time since the last shot is more than
+            // the required delay.
+            if (this.game.time.now - this.lastBulletShotAt < SHOT_DELAY) return;
+            this.lastBulletShotAt = this.game.time.now;
+
+            this.bulletPool.children[iBullet++%NUMBER_OF_BULLETS].shootBullet(0,0);
+        }
     }
 };
