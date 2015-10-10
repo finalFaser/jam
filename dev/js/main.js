d9977985daaaff2bead84f69f82c8efcc7ea79e6
Anmo <btavares26@gmail.com>
2015-10-10 13:37:59 +0100

    canvas 600x600 center (again)

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,15 @@
 "use strict";
 
-var game = new Phaser.Game(1024, 512);
+window.game = new Phaser.Game(600, 600, Phaser.AUTO);
+
+game.globals = {
+    //Add variables here that you want to access globally
+    //score: 0 could be accessed as game.globals.score for example
+};
+
+game.state.add('play', require('./states/play.js'));
+game.state.add('load', require('./states/load.js'));
+game.state.add('menu', require('./states/menu.js'));
+game.state.add('boot', require('./states/boot.js'));
+
+game.state.start('boot');
