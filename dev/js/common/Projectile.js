d357008608826ec50e2b00055da193d9b982b694
unknown <zeratul.exe@gmail.com>
2015-10-10 17:49:13 +0100

    fire the cannons!

--- dev/js/common/Projectile.js
+++ dev/js/common/Projectile.js
@@ -2,17 +2,17 @@
  * Created by jorge.graca on 10/10/2015.
  */
 
-var Projectile = function(game){
-    //var arcade = new Phaser.Arcade(game);
+// Define constants
+var SHOT_DELAY = 100; // milliseconds (10 bullets/second)
+var BULLET_SPEED = 500; // pixels/second
+var NUMBER_OF_BULLETS = 1;
 
+var Projectile = function(game){
     Phaser.Group.call( this, game );
 
-    this.coisa = this.create(300, 300, 'BalaPotassio');
-    this.coisa.anchor.set(0.5,0.5);
-
-    //player movement
-    //this.coisa.body.velocity.y = 0;
-    //this.coisa.body.velocity.x = 0;
+    this.bullet = this.create(300, 300, 'BalaPotassio');
+    this.game.physics.enable(this.bullet, Phaser.Physics.ARCADE);
+    this.bullet.anchor.set(0.5,0.5);
 };
 
 Projectile.prototype = Object.create(Phaser.Group.prototype);
@@ -20,18 +20,49 @@ Projectile.prototype = Object.create(Phaser.Group.prototype);
 Projectile.prototype.update = function() {
     Phaser.Group.prototype.update.call(this);
 
-/*    if(this.cursors.up.isDown) {
-        this.player.body.velocity.y -= 50;
+    // Shoot a bullet
+    if (this.game.input.activePointer.isDown) {
+        this.shootBullet();
     }
-    else if(this.cursors.down.isDown) {
-        this.player.body.velocity.y += 50;
-    }
-    if(this.cursors.left.isDown) {
-        this.player.body.velocity.x -= 50;
-    }
-    else if(this.cursors.right.isDown) {
-        this.player.body.velocity.x += 50;
-    }*/
+};
+
+Projectile.prototype.shootBullet = function(rotation) {
+    // Enforce a short delay between shots by recording
+    // the time that each bullet is shot and testing if
+    // the amount of time since the last shot is more than
+    // the required delay.
+    if (this.lastBulletShotAt === undefined) this.lastBulletShotAt = 0;
+    if (this.game.time.now - this.lastBulletShotAt < this.SHOT_DELAY) return;
+    this.lastBulletShotAt = this.game.time.now;
+
+    // Get a dead bullet from the pool
+    //var bullet = this.bulletPool.getFirstDead();
+
+    // If there aren't any bullets available then don't shoot
+    //if (bullet === null || bullet === undefined) return;
+
+    // Revive the bullet
+    // This makes the bullet "alive"
+    //bullet.revive();
+
+    // Bullets should kill themselves when they leave the world.
+    // Phaser takes care of this for me by setting this flag
+    // but you can do it yourself by killing the bullet if
+    // its x,y coordinates are outside of the world.
+    this.bullet.checkWorldBounds = true;
+    this.bullet.outOfBoundsKill = true;
+
+    // Set the bullet position to the gun position.
+    //bullet.reset(this.gun.x, this.gun.y);
+
+    // Aim the gun at the pointer.
+    // All this function does is calculate the angle using
+    // Math.atan2(yPointer-yGun, xPointer-xGun)
+    this.bullet.rotation = this.game.physics.arcade.angleToPointer(this.bullet);
+
+    // Shoot it
+    this.bullet.body.velocity.x = Math.cos(this.bullet.rotation) * BULLET_SPEED;
+    this.bullet.body.velocity.y = Math.sin(this.bullet.rotation) * BULLET_SPEED;
 };
 
 Projectile.prototype.constructor = Projectile;
