07702c947b9d12cd9a70bdf9eb641cb5d1d55e33
unknown <zeratul.exe@gmail.com>
2015-10-11 17:35:35 +0100

    scientist refactor

--- dev/js/states/play.js
+++ dev/js/states/play.js
@@ -9,8 +9,22 @@ module.exports = {
     create: function(){
 
         this.background = this.add.sprite(0, 0, 'background');
-
         this.themeSound = this.game.add.audio('themeSound', 0.05, true).play();
+
+        this._scientist = this.add.sprite(300, 300, 'scientist');
+        this._scientist.scale= {x:2, y:2};
+        this._scientist.anchor.set(0.5, 0.5);
+
+        this._scientist.animations.add('walk', [1, 2]);
+        this._scientist.animations.add('run', [11, 12, 13, 14]);
+        this._scientist.animations.add('attack', [19, 20, 21, 31, 32]);
+
+        this.game.physics.arcade.enable(this._scientist);
+
+        this._cursors = this.game.input.keyboard.createCursorKeys();
+        this._shift = this.game.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
+
+        this._isAttacking = 0;
 
         this.lastLazerShotAt = 0;
 
@@ -32,24 +46,6 @@ module.exports = {
             // Kill the bullet
             obj2.sprite.kill();
         }, this);
-
-
-
-
-
-        this._barbarian = this.add.sprite(300, 300, 'scientist');
-        this._barbarian.anchor.set(0.5, 0.5);
-
-        this._barbarian.animations.add('walk', [0, 1, 2, 3, 4, 5, 6, 7, 8]);
-        this._barbarian.animations.add('run', [11, 12, 13, 14, 15, 16, 17, 18]);
-        this._barbarian.animations.add('attack', [19, 20, 21]);
-
-        this.game.physics.arcade.enable(this._barbarian);
-
-        this._cursors = this.game.input.keyboard.createCursorKeys();
-        this._shift = this.game.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
-
-        this._isAttacking = 0;
     },
     update: function(){
 
@@ -62,47 +58,49 @@ module.exports = {
             }
         }
 
-        this._barbarian.body.velocity.x = 0;
-        this._barbarian.body.velocity.y = 0;
+        this._scientist.body.velocity.x = 0;
+        this._scientist.body.velocity.y = 0;
 
         if (this._isAttacking) {
             //others have magic mike
             //we have magic numbers
-            this._barbarian.animations.play('attack', 10);
+            if (this.game.time.now - this.lastLazerShotAt > LAZER_DELAY) {
+                this._scientist.animations.play('attack', 10);
+            }
         } else if (this._cursors.left.isDown || this._cursors.right.isDown || this._cursors.up.isDown || this._cursors.down.isDown) {
-            this._barbarian.animations.play(this._shift.isDown ? 'run' : 'walk', 30, true);
+            this._scientist.animations.play(this._shift.isDown ? 'run' : 'walk', 30, true);
         } else {
-            this._barbarian.animations.stop('walk');
-            this._barbarian.animations.stop('run');
-            this._barbarian.frame = 0;
+            this._scientist.animations.stop('walk');
+            this._scientist.animations.stop('run');
+            this._scientist.frame = 0;
         }
 
         if (this._isAttacking && this._isAttacking--) {
             //others have magic mike
             //we have magic numbers
             if (this._isAttacking === 8) {
-                //this.bulletPool.shoot(this._barbarian.x + 60, this._barbarian.y - 15);
+                //this.bulletPool.shoot(this._scientist.x + 60, this._scientist.y - 15);
                 this.bulletPool2.shoot(600, 600);
             }
 
             if (this.game.time.now - this.lastLazerShotAt > LAZER_DELAY) {
                 this.lastLazerShotAt = this.game.time.now;
-                this.beam.shoot(this._barbarian.x, this._barbarian.y);
+                this.beam.shoot(this._scientist.x, this._scientist.y);
             }
         }
 
         var speedModifier = this._shift.isDown ? 2 : 1;
 
         if (this._cursors.left.isDown) {
-            this._barbarian.body.velocity.x = speedModifier * -200;
+            this._scientist.body.velocity.x = speedModifier * -200;
         } else if (this._cursors.right.isDown) {
-            this._barbarian.body.velocity.x = speedModifier * 200;
+            this._scientist.body.velocity.x = speedModifier * 200;
         }
 
         if (this._cursors.up.isDown) {
-            this._barbarian.body.velocity.y = speedModifier * -200;
+            this._scientist.body.velocity.y = speedModifier * -200;
         } else if (this._cursors.down.isDown) {
-            this._barbarian.body.velocity.y = speedModifier * 200;
+            this._scientist.body.velocity.y = speedModifier * 200;
         }
     }
 };
